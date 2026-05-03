ba20ae4f61556c432adee4579deb0817b8a5375d
jeeva-toro <jeeva.toro@gmail.com>
2026-05-03 12:37:18 +0530

    bug fix2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import heroImage from '../assets/serene-bedroom.png'
 import FadeInSection from './FadeInSection'
 import ParallaxImage from './ParallaxImage'
